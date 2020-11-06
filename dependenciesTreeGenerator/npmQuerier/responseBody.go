2bccd5df5abdba4f6aaf434020e17a9141245df1
ornovog <or.novog@gmail.com>
2020-11-06 12:01:15 +0200

    remove unnecessary fields from responseBody

--- dependenciesTreeGenerator/npmQuerier/responseBody.go
+++ dependenciesTreeGenerator/npmQuerier/responseBody.go
@@ -2,7 +2,5 @@ package npmQuerier
 
 type responseBody struct {
 	Name        string            `json:"name"`
-	Description string            `json:"description"`
-	DistTags    map[string]string `json:"dist-tags"`
 	Package     packageData       `json:"versions"`
 }
